27c1124d51b35f89e58dca8a73d6acb389082dad
Bradley Campbell <bradley@Bradleys-MacBook-Pro.local>
2017-02-08 14:03:26 -0700

    added and successfully ran post validation testing

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  before do
+    @post = Post.create!(date: Date.today, rationale: "Anything")
+  end
+  describe "creation" do
+    it "can be created" do
+      
+      expect(@post).to be_valid
+    end
+
+    it "cannot be created without a date and rationale" do
+      @post.date = nil
+      @post.rationale = nil
+      expect(@post).to_not be_valid
+    end
+  end
+
+end
