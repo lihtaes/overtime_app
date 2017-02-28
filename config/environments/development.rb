7bc59b19a30d562c2e8b954c44d58c1aee35ca7b
Bradley Campbell <bb.campbell606@gmail.com>
2017-02-27 20:57:35 -0700

    implemented basic email functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.active_record.migration_error = :page_load
